15c170fd54e5f564738e19ffbc27c3593c2ac68e
Igor Vilhovy <ukrainian12@gmail.com>
2018-04-22 21:49:08 +0300

    [+] Human can play with computer.

--- TicTacToeAI/Main.cpp
+++ TicTacToeAI/Main.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <random>
 #include <string>
 using namespace std;
 
 void generateStatistics();
 bool checkWin(const string grid, const char winner);
+void countOutcomes(const string grid, const char player, int& wins, int& losses);
+string computeMove(const string grid, const char playerWhoMakesMove);
+bool randomCheck();
 
-const char FREE = '#';
+const char FREE = ' ';
 const char XS = 'X';
 const char OS = 'O';
 
-// Represent the game grid as a string of Xs and Os.
-string grid;
 const string emptyGrid = string(9, FREE);
 
 vector<string> statistics;
@@ -47,7 +49,7 @@ void generateStatistics()
 
 			char winner = (isXWinner ? XS : OS);
 
-			statistics.push_back(currentGrid + winner + firstMoveMake);
+			statistics.push_back(currentGrid + firstMoveMake + winner);
 		}
 
 		overflow = false;
@@ -96,7 +98,123 @@ bool checkWin(const string grid, const char winner)
 		(g[2] == winner && g[4] == winner && g[6] == winner);
 }
 
+string computeMove(const string grid, const char playerWhoMakesMove)
+{
+	int minLosses = -1;
+	int maxWins = -1;
+	string result = grid;
+
+	for (int p = 0; p < 9; p++)
+	{
+		if (grid[p] == FREE)
+		{
+			string gridWithMoveInP = grid;
+			gridWithMoveInP[p] = playerWhoMakesMove;
+			
+			int lossesForP = 0, winsForP = 0;
+			countOutcomes(gridWithMoveInP, playerWhoMakesMove, winsForP, lossesForP);
+
+			if (minLosses == -1 ||
+				lossesForP < minLosses ||
+				lossesForP == minLosses && winsForP > maxWins ||
+				lossesForP == minLosses && winsForP == maxWins && randomCheck())
+			{
+				minLosses = lossesForP;
+				maxWins = winsForP;
+				result = gridWithMoveInP;
+			}
+		}
+	}
+
+	return result;
+}
+
+bool randomCheck()
+{
+	static uniform_int_distribution<> dist(0, 1);
+	return dist(default_random_engine()) != 0;
+}
+
+void countOutcomes(const string grid, const char player, int& wins, int& losses)
+{
+	wins = losses = 0;
+	const string& g = grid;
+
+	for (const string outcome : statistics)
+	{
+		const string& o = outcome;
+
+		if ((o[9] == g[9]) &&
+			(g[0] == FREE || g[0] == o[0]) &&
+			(g[1] == FREE || g[1] == o[1]) &&
+			(g[2] == FREE || g[2] == o[2]) &&
+			(g[3] == FREE || g[3] == o[3]) &&
+			(g[4] == FREE || g[4] == o[4]) &&
+			(g[5] == FREE || g[5] == o[5]) &&
+			(g[6] == FREE || g[6] == o[6]) &&
+			(g[7] == FREE || g[7] == o[7]) &&
+			(g[8] == FREE || g[8] == o[8]))
+		{
+			if (o[10] != player)// outcome[10] indicate a winner.
+			{
+				losses++;
+			}
+			else
+			{
+				wins++;
+			}
+		}
+	}
+}
+
 int main()
 {
 	generateStatistics();
+
+	// Represent the game grid as a string of Xs and Os.
+	// The "+ XS" at the end of the string
+	// encodes who makes the first move.
+	string grid = emptyGrid + XS;
+	char whoseTurn = XS;
+	for (int i = 0; i < 9; i++)
+	{
+		if (whoseTurn == OS)
+		//if (whoseTurn == OS || whoseTurn == XS)
+		{
+			grid = computeMove(grid, whoseTurn);
+		}
+		else
+		{
+			int x, y;
+			bool problemWithInput = false;
+			do
+			{
+				cin >> x >> y;
+				problemWithInput =
+					x < 0 || x > 2 ||
+					y < 0 || y > 2 ||
+					grid[x + y * 3] != FREE;
+
+			} while (problemWithInput);
+
+			grid[x + y * 3] = whoseTurn;
+		}
+
+		const string& g = grid;
+		cout << g[0] << g[1] << g[2] << endl
+			<< g[3] << g[4] << g[5] << endl
+			<< g[6] << g[7] << g[8] << endl
+			<< "---\n";
+
+		if (checkWin(grid, whoseTurn))
+		{
+			cout << whoseTurn << " win..\n";
+			return 0;
+		}
+
+		whoseTurn = (whoseTurn == XS ? OS : XS);
+	}
+
+	cout << "It is draw..\n";
+	return 0;
 }
