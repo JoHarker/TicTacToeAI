0194c5274659cb6e95050d146ce88345fc4c9192
Igor Vilhovy <ukrainian12@gmail.com>
2018-05-02 02:27:27 +0300

    [-] Removed the AI behavior based on the outcomes counting. It doesn't works. [+] Added the minimax behavior.

--- TicTacToeAI/Main.cpp
+++ TicTacToeAI/Main.cpp
@@ -1,92 +1,131 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <random>
 #include <string>
+#include <algorithm>
 using namespace std;
 
-void generateStatistics();
-bool checkWin(const string grid, const char winner);
-void countOutcomes(const string grid, const char player, int& wins, int& losses);
 string computeMove(const string grid, const char playerWhoMakesMove);
+int calculateMoveEstimate(const string grid, const char playerToMaximize, const bool isMaximizingPlayerMove);
+bool checkWin(const string grid, const char winner);
+bool checkNoMoreMovesLeft(const string grid);
 bool randomCheck();
 
 const char FREE = ' ';
 const char XS = 'X';
 const char OS = 'O';
 
+const int WIN_ESTIMATE = +1;
+const int LOOSE_ESTIMATE = -1;
+const int DRAW_ESTIMATE = 0;
+
 const string emptyGrid = string(9, FREE);
 
-vector<string> statistics;
-
-void generateStatistics()
+string computeMove(const string grid, const char playerWhoMakesMove)
 {
-	// Lets suppose FREE is a 0, XS is a 1 and OS is a 2. 
-	// Then the grid cell is a ternary digit and it is't complicated 
-	// to go through all game cases.
-	string currentGrid = emptyGrid;
-	bool overflow;
-	do
+	int bestMove;
+	int bestEstimate;
+	bool isBestEstimateSet = false;
+
+	for (int move = 0; move < 9; move++)
 	{
-		int numberXs = 0, numberOs = 0;
-		for (int i = 0; i < 9; i++)
+		if (grid[move] == FREE)
 		{
-			if (currentGrid[i] == XS) numberXs++;
-			else if (currentGrid[i] == OS) numberOs++;
-		}
+			string gridWithMove = grid;
+			gridWithMove[move] = playerWhoMakesMove;
+			int estimateForMove = calculateMoveEstimate(gridWithMove, playerWhoMakesMove, false);
 
-		bool isXWinner = checkWin(currentGrid, XS);
-		bool isOWinner = checkWin(currentGrid, OS);
-
-		if ((abs(numberXs - numberOs) == 0 || abs(numberXs - numberOs) == 1) &&
-			(isXWinner && !isOWinner || !isXWinner && isOWinner) &&
-			(isXWinner && numberXs >= numberOs || isOWinner && numberOs >= numberXs))
-		{
-			char firstMoveMake;
-			if (isXWinner) 
-				firstMoveMake = (numberXs == numberOs ? OS : XS);
-			if (isOWinner) 
-				firstMoveMake = (numberXs == numberOs ? XS : OS);
-
-			char winner = (isXWinner ? XS : OS);
-
-			statistics.push_back(currentGrid + firstMoveMake + winner);
-		}
-
-		overflow = false;
-		for (int i = 8; i >= 0; i--)
-		{
-			if (currentGrid[i] == FREE)
+			if (!isBestEstimateSet || 
+				estimateForMove > bestEstimate ||
+				estimateForMove == bestEstimate && randomCheck())
 			{
-				currentGrid[i] = XS;
-				break;
+				bestEstimate = estimateForMove;
+				bestMove = move;
 			}
-			else if (currentGrid[i] == XS)
+			
+			isBestEstimateSet = true;
+		}
+	}
+
+	string result = grid;
+	result[bestMove] = playerWhoMakesMove;
+
+	return result;
+}
+
+map<tuple<string, char, bool>, int> estimatesMemory;
+
+int calculateMoveEstimate(const string grid, const char playerToMaximize, const bool isMaximizingPlayerMove)
+{
+	const char anotherPlayer = (playerToMaximize == XS ? OS : XS);
+	const auto state = tuple<string, char, bool>(grid, playerToMaximize, isMaximizingPlayerMove);
+
+	if (estimatesMemory.find(state) != estimatesMemory.end())
+	{
+		return estimatesMemory[state];
+	}
+	
+	if (checkWin(grid, playerToMaximize))
+	{
+		estimatesMemory[state] = WIN_ESTIMATE;
+		return WIN_ESTIMATE;
+	}
+	else if (checkWin(grid, anotherPlayer))
+	{
+		estimatesMemory[state] = LOOSE_ESTIMATE;
+		return LOOSE_ESTIMATE;
+	}
+	else if (checkNoMoreMovesLeft(grid))
+	{
+		estimatesMemory[state] = DRAW_ESTIMATE;
+		return DRAW_ESTIMATE;
+	}
+	else
+	{
+		int estimate;
+		bool isEstimateSet = false;
+
+		for (int move = 0; move < 9; move++)
+		{
+			if (grid[move] == FREE)
 			{
-				currentGrid[i] = OS;
-				break;
-			}
-			else if (currentGrid[i] == OS)
-			{
-				if (i - 1 < 0)
+				string gridWithMove = grid;
+				gridWithMove[move] = (isMaximizingPlayerMove ? playerToMaximize : anotherPlayer);
+				int estimateForMove = calculateMoveEstimate(gridWithMove, playerToMaximize, !isMaximizingPlayerMove);
+
+				if (!isEstimateSet)
 				{
-					overflow = true;
-					break;
+					estimate = estimateForMove;
+					isEstimateSet = true;
 				}
 				else
 				{
-					currentGrid[i] = FREE;
-					continue;
+					if (isMaximizingPlayerMove)
+					{
+						estimate = max(estimate, estimateForMove);
+					}
+					else
+					{
+						estimate = min(estimate, estimateForMove);
+					}
 				}
+
+				if (isMaximizingPlayerMove && estimate == WIN_ESTIMATE ||
+					!isMaximizingPlayerMove && estimate == LOOSE_ESTIMATE)
+					break;
 			}
 		}
 
-	} while (!overflow);
+		estimatesMemory[state] = estimate;
+		return estimate;
+	}
 }
 
 bool checkWin(const string grid, const char winner)
 {
 	const string& g = grid;
-	
+
 	return
 		(g[0] == winner && g[1] == winner && g[2] == winner) ||
 		(g[3] == winner && g[4] == winner && g[5] == winner) ||
@@ -98,88 +137,33 @@ bool checkWin(const string grid, const char winner)
 		(g[2] == winner && g[4] == winner && g[6] == winner);
 }
 
-string computeMove(const string grid, const char playerWhoMakesMove)
+bool checkNoMoreMovesLeft(const string grid)
 {
-	int minLosses = -1;
-	int maxWins = -1;
-	string result = grid;
+	const string& g = grid;
 
-	for (int p = 0; p < 9; p++)
-	{
-		if (grid[p] == FREE)
-		{
-			string gridWithMoveInP = grid;
-			gridWithMoveInP[p] = playerWhoMakesMove;
-			
-			int lossesForP = 0, winsForP = 0;
-			countOutcomes(gridWithMoveInP, playerWhoMakesMove, winsForP, lossesForP);
-
-			if (minLosses == -1 ||
-				lossesForP < minLosses ||
-				lossesForP == minLosses && winsForP > maxWins ||
-				lossesForP == minLosses && winsForP == maxWins && randomCheck())
-			{
-				minLosses = lossesForP;
-				maxWins = winsForP;
-				result = gridWithMoveInP;
-			}
-		}
-	}
-
-	return result;
+	return
+		g[0] != FREE && g[1] != FREE && g[2] != FREE &&
+		g[3] != FREE && g[4] != FREE && g[5] != FREE &&
+		g[6] != FREE && g[7] != FREE && g[8] != FREE;
 }
 
 bool randomCheck()
 {
+	static random_device rd;
+	static mt19937 gen(rd());
 	static uniform_int_distribution<> dist(0, 1);
-	return dist(default_random_engine()) != 0;
-}
-
-void countOutcomes(const string grid, const char player, int& wins, int& losses)
-{
-	wins = losses = 0;
-	const string& g = grid;
-
-	for (const string outcome : statistics)
-	{
-		const string& o = outcome;
-
-		if ((o[9] == g[9]) &&
-			(g[0] == FREE || g[0] == o[0]) &&
-			(g[1] == FREE || g[1] == o[1]) &&
-			(g[2] == FREE || g[2] == o[2]) &&
-			(g[3] == FREE || g[3] == o[3]) &&
-			(g[4] == FREE || g[4] == o[4]) &&
-			(g[5] == FREE || g[5] == o[5]) &&
-			(g[6] == FREE || g[6] == o[6]) &&
-			(g[7] == FREE || g[7] == o[7]) &&
-			(g[8] == FREE || g[8] == o[8]))
-		{
-			if (o[10] != player)// outcome[10] indicate a winner.
-			{
-				losses++;
-			}
-			else
-			{
-				wins++;
-			}
-		}
-	}
+	bool check = dist(gen) != 0;
+	return check;
 }
 
 int main()
 {
-	generateStatistics();
-
-	// Represent the game grid as a string of Xs and Os.
-	// The "+ XS" at the end of the string
-	// encodes who makes the first move.
-	string grid = emptyGrid + XS;
+	string grid = emptyGrid;
 	char whoseTurn = XS;
 	for (int i = 0; i < 9; i++)
 	{
 		if (whoseTurn == OS)
-		//if (whoseTurn == OS || whoseTurn == XS)
+		//	if (whoseTurn == OS || whoseTurn == XS)
 		{
 			grid = computeMove(grid, whoseTurn);
 		}
